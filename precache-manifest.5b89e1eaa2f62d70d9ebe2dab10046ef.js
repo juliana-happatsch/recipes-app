64de64c8a2d762a4bdb15a269638fb155008ee75
Juliana Happatsch <jhappatsch@gmail.com>
2021-08-16 16:46:46 -0300

    Updates

---
+++ precache-manifest.5b89e1eaa2f62d70d9ebe2dab10046ef.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc976bb1ea5bb95813a09fcc9e8a2a81",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "b73fae4d401723d350de",
+    "url": "/recipes-app/static/css/main.30397132.chunk.css"
+  },
+  {
+    "revision": "aafd9143962491550374",
+    "url": "/recipes-app/static/js/2.f6ea022b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/recipes-app/static/js/2.f6ea022b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73fae4d401723d350de",
+    "url": "/recipes-app/static/js/main.69afb38b.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "367f1eede0d16f4d41a7b063ad21957e",
+    "url": "/recipes-app/static/media/bx-arrow-back.367f1eed.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "412cd5645a8ae0968b7aca45c5f94e21",
+    "url": "/recipes-app/static/media/error404.412cd564.png"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "8e1e94e261c32bf3cd3ebd836303ecdf",
+    "url": "/recipes-app/static/media/fundinho.8e1e94e2.png"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
